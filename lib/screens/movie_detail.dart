e2da18c435a3a8d15704ab327267883dcba5ed7d
unknown <bayrsaikhan.ger13@gmail.com>
2024-05-06 11:50:22 +0800

    feat: movie detail done

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -1,12 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/Model/movie/index.dart';
 
 class MovieDetailPage extends StatelessWidget {
-  const MovieDetailPage({super.key});
+  final MovieModel data;
+  const MovieDetailPage(this.data, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Movie Detail Page"),
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 34, 36, 40),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(data.title),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Stack(
+                  // fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                      data.imgUrl,
+                      width: width,
+                      fit: BoxFit.fill,
+                    ),
+                    Container(
+                      color: Colors.black.withOpacity(0.8),
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      left: 0,
+                      right: 0,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.play_circle,
+                            color: Colors.grey.withOpacity(0.9),
+                            size: 60,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            data.title,
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white.withOpacity(0.9),
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "${data.publishedYear} | ${data.durationMin} | ${data.type}",
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white.withOpacity(0.8)),
+                          )
+                        ],
+                      ),
+                    ),
+                    SafeArea(
+                        child: Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: Icon(
+                          Icons.chevron_left,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                    ))
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Тайлбар",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      data.description ?? " ",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w600),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/widgets/movie_special_card.dart
+++ lib/widgets/movie_special_card.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:movieapp/Model/movie/index.dart';
+import 'package:movieapp/screens/movie_detail.dart';
 
 class MovieSpecialCard extends StatelessWidget {
   final MovieModel data;
 
   const MovieSpecialCard(this.data, {super.key});
 
+  void _onCardTap(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (_) => MovieDetailPage(data)));
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width * 0.5;
-    return Container(
-      height: width * 1.5,
-      width: width,
-      margin: EdgeInsets.only(right: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        image: DecorationImage(
-            image: NetworkImage(data.imgUrl), fit: BoxFit.cover),
-      ),
-      child: Icon(
-        Icons.play_circle,
-        color: Colors.grey.withOpacity(0.8),
-        size: 60,
-      ),
-    );
+    return InkWell(
+        onTap: () => _onCardTap(context),
+        child: Container(
+          height: width * 1.5,
+          width: width,
+          margin: EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+                image: NetworkImage(data.imgUrl), fit: BoxFit.cover),
+          ),
+          child: Icon(
+            Icons.play_circle,
+            color: Colors.grey.withOpacity(0.8),
+            size: 60,
+          ),
+        ));
   }
 }
